5e27e150737d6212d018a15599d8c835ed9f3599
Matthew Uchytil <Muchytil@outlook.com>
2021-06-14 12:41:02 -0500

    add main page

--- Unitas/Data/Address.cs
+++ Unitas/Data/Address.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,16 @@ namespace Unitas.Data
 {
     public class Address
     {
+        [Required]
         public int Id { get; set; }
+
         public string Street { get; set; }
+
         public string City { get; set; }
+        [Required]
+        [MaxLength(2)]
         public string State { get; set; }
+        [Required]
         public string Country { get; set; }
         public int Zipcode { get; set; }
     }
